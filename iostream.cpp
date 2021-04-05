4eb41e88bd71c6c46a35f525e86ae548bc692c99
ivan-boginski <aisivan36@gmail.com>
2021-04-05 10:34:03 +0700

    get into deep

--- iostream.cpp
+++ iostream.cpp
@@ -8,11 +8,32 @@ int main()
     std::string enter;
     std::getline(std::cin, enter);
 
-    std::fstream fs{enter};
-    std::string s;
-    while (fs)
+    // std::fstream fs{enter};
+    // std::string s;
+    std::string esnter;
+
+    // for write
+    std::cout << "Write something to this file: : ";
+    std::string writes;
+    std::getline(std::cin, writes);
+    std::fstream fs{
+        enter,
+        std::ios::app};
+    // write
+    fs << writes;
+    // Read
+    std::string writes;
+
+    // fs >> enter;
+
+    // //     // Getline for drawing on the same line
+    // std::getline(fs, enter);
+    // std::cout << enter << '\n';
+
+    while (fs >> writes)
     {
-        std::getline(fs, s);
-        std::cout << s << '\n';
+        // Getline for drawing on the same line
+        std::getline(fs, writes);
+        std::cout << writes << '\n';
     }
 }
