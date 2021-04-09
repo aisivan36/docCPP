1cec4733086e1f40709853de69435cd6fdfd25f3
ivan-boginski <aisivan36@gmail.com>
2021-04-10 05:28:32 +0700

    moved

--- lib/mymod.cpp
+++ lib/mymod.cpp
@@ -1,3 +1,13 @@
-export module mymodule;
-
-export double thefunction() { return 456.789 }
+#include <iostream>
+#include <ranges>
+#include <vector>
+#include <algorithm>
+int main()
+{
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    auto greaterthan2view = v | std::views::filter([](int x) { return x > 2; });
+    for (auto el : greaterthan2view)
+    {
+        std::cout << el << '\n';
+    }
+}
