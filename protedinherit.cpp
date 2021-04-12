d5bcd311be4bbedadf7e663008a69df8ebd6d6c3
ivan boginski <aisivan36@gmail.com>
2021-04-12 15:12:56 +0700

    Never use break for nested loop

--- protedinherit.cpp
+++ protedinherit.cpp
@@ -91,6 +91,7 @@ void sure() {
     std::cout << "see you!\n";
     return; // stop at here
     
+//     Do not use break on this scope. keep that in mind.
 //     break; // break keyword: Is used to terminate the execution of any looping structures(for loop, while, do_while) immediately. If it is used inside in function or method, it will not terminate the function just the looping structure will be terminated
   }
 }
