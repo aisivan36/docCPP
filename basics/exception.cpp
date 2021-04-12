dce82344567d70f66648ae1677c5cbeaf8977a31
ivan-boginski <aisivan36@gmail.com>
2021-04-12 21:56:24 +0700

    try catch block

--- basics/exception.cpp
+++ basics/exception.cpp
@@ -2,20 +2,24 @@
 using namespace std;
 
 int main() {
-  int input;
-  cout << "How old are you?: " << '\n';
-  cin >> input;
+  //   int input;
+
   try {
     int input;
+    cout << "How old are you?: " << '\n';
+    cin >> input;
     if (input >= 18) {
       cout << "Access granted - you are old enough.";
       return 0;
     } else {
       throw(input);
     }
-  } catch (int myNum) {
+  } catch (...) // use ... dot three times if you do not know what the type
+                // inside try block is.
+  {
     cout << "Access denied - You must be at least 18 years old.\n";
-    cout << "Age is: " << myNum;
+    // cout << "Age is: " << myNum; // commenting out to use variable and define
+    // parameter as int or appropriate data type inside the try block.
   }
   return 0;
 }
