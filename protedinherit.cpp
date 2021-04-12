2b843659ddc2178533e43186ab6b3c5624e8220e
ivan-boginski <aisivan36@gmail.com>
2021-04-12 12:07:22 +0700

    simple question program

--- protedinherit.cpp
+++ protedinherit.cpp
@@ -21,13 +21,47 @@ public:
   int getExpr() { return expr; }
 };
 
-int main() {
+class Question {
+public:
+  void question() { std::cout << "Are you programmer? (y/n): "; }
+};
+
+void game() {
+  char input;
+
   Person TheJob;
   TheJob.setJob("Rust");
   TheJob.status = "Leader";
   TheJob.minExper(5);
+  Question ask;
+  do {
+    ask.question();
 
-  std::cout << "Skill: " << TheJob.getJob() << '\n';
-  std::cout << "Status: " << TheJob.status << '\n';
-  std::cout << "Experienced: " << TheJob.getExpr() << " Year(s)\n";
+    std::cin >> input;
+    if (input == 'y') {
+      std::cout << "Skill: " << TheJob.getJob() << '\n';
+      std::cout << "Status: " << TheJob.status << '\n';
+      std::cout << "Experienced: " << TheJob.getExpr() << " Year(s)\n";
+    } else if (input == 'n') {
+      std::cout << "Have a nice day sir!\n";
+      break;
+    }
+
+  } while (input != 'y' && 'n');
+}
+
+int main() {
+  game();
+
+  char ask;
+  do {
+    std::cout << "Would you like to play again? (y/n): ";
+    std::cin >> ask;
+
+    if (ask == 'n') {
+      break;
+    }
+    game();
+
+  } while (ask == 'y');
 }
