def991f1fb2b5fc4941977ead2234824e74b70ff
ivan-boginski <aisivan36@gmail.com>
2021-04-12 11:05:34 +0700

    protected class multiple inherit

--- protedinherit.cpp
+++ protedinherit.cpp
@@ -6,18 +6,28 @@ protected:
   std::string skill;
 };
 
-class Person : public Job {
+class Experienced {
+protected:
+  int expr;
+};
+
+class Person : public Job, public Experienced {
 public:
   std::string status;
   void setJob(std::string j) { skill = j; }
   std::string getJob() { return skill; }
+
+  void minExper(int ex) { expr = ex; }
+  int getExpr() { return expr; }
 };
 
 int main() {
   Person TheJob;
   TheJob.setJob("Rust");
   TheJob.status = "Leader";
+  TheJob.minExper(5);
 
-  std::cout << "Skill" << TheJob.getJob() << '\n';
-  std::cout << "Status" << TheJob.status << '\n';
+  std::cout << "Skill: " << TheJob.getJob() << '\n';
+  std::cout << "Status: " << TheJob.status << '\n';
+  std::cout << "Experienced: " << TheJob.getExpr() << " Year(s)\n";
 }
