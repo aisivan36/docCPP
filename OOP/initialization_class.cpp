df148a7ff2890abc0bcc8e6f0f42a5ececf2c6f6
ivan-boginski <aisivan36@gmail.com>
2021-04-18 21:54:39 +0700

    Initialization Class and Accessing Class Method

--- OOP/initialization_class.cpp
+++ OOP/initialization_class.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 class Player {
 private:
@@ -11,7 +12,18 @@ public:
   Player();
   Player(std::string name_val);
   Player(std::string name_val, int health_val, int xp_val);
+
+  std::string car;
+  int price;
+  int year;
+  void listen(std::string say_something) {
+    std::cout << name << " speaks " << say_something << std::endl;
+  }
 };
+
+// class NewClass {
+
+// };
 
 Player::Player() : name{"None"}, health{0}, xp{0} {}
 
@@ -21,8 +33,21 @@ Player::Player(std::string name_val, int health_val, int xp_val)
     : name{name_val}, health{health_val}, xp{xp_val} {}
 
 int main() {
+
   Player empty;
   Player frank{"Frank"};
   Player villain{"Ivan"};
+
+  // Accessing Struct or Class member
+  Player *Pointer = new Player;
+  // Can be either
+  (*Pointer).car = "Peugeot";
+  // Or with this arrow operator
+  Pointer->price = 3;
+  Pointer->year = 1;
+  std::cout << "Price: " << Pointer->price << "$ Age: " << Pointer->year
+            << " Year-old. Brand: " << Pointer->car << '\n';
+  Pointer->listen("Nothing to say");
+
   return 0;
 }
