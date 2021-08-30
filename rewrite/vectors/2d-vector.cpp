c8da840de18d7facc2e2e607b4160ae98a9e48ff
ivan-boginski <aisivan36@gmail.com>
2021-08-30 08:54:12 +0700

    ..

--- rewrite/vectors/2d-vector.cpp
+++ rewrite/vectors/2d-vector.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <vector>
-// using namespace std;
+using namespace std;
+
+void trefunc() {
+  vector<vector<int>> random{{1, 2}, {4, 5, 6}, {7, 8, 9, 10}};
+
+  cout << random.size() << endl;
+
+  for (int i = 0; i < random.size(); i++) {
+    for (int j = 0; j < random[i].size(); j++) {
+      // cout << "\nThis is a J: " << j << endl;
+      cout << random[i][j];
+    }
+    cout << endl;
+  }
+}
 
 int main() {
+  trefunc();
+  cout << "===============================\n";
+
   std::vector<std::vector<int>> vector{
       {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
